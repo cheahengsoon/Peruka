bf69de72d1eb69eae22bd321cb0c8d3f511b657b
Pekka Heikura <pekkah@gmail.com>
2013-08-11 11:16:33 +0300

    Configure WebApi
    Configure WebApi with default settings

---
+++ src/Peruka.Web/Api/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿namespace Peruka.Web.Api
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class HelloWorldController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello world!");
+        }
+    }
+}
